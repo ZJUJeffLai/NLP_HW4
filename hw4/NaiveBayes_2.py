ad134031854883b17f362e4f9cfaaa639936dbee
Jeff Lai <zjdxlzf@163.com>
2019-05-29 16:14:00 -0700

    2

---
+++ hw4/NaiveBayes_2.py
@@ -0,0 +1,390 @@
+import sys
+import getopt
+import os
+import math
+import operator
+from collections import defaultdict
+
+negWords = ['no','not','never','terrible','bad','worse','worst','waste','disappointed']
+punctuations = '!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'
+
+turnWords = ['although','though']
+
+class NaiveBayes:
+    class TrainSplit:
+        """
+        Set of training and testing data
+        """
+        def __init__(self):
+            self.train = []
+            self.test = []
+
+    class Document:
+        """
+        This class represents a document with a label. classifier is 'pos' or 'neg' while words is a list of strings.
+        """
+        def __init__(self):
+            self.classifier = ''
+            self.words = []
+
+    def __init__(self):
+        """
+        Initialization of naive bayes
+        """
+        self.stopList = set(self.readFile('data/english.stop'))
+        self.bestModel = False
+        self.stopWordsFilter = False
+        self.naiveBayesBool = False
+        self.numFolds = 10
+
+        ###
+        self.classifiers = defaultdict(lambda: 0)
+        self.nDocuments = 0
+        self.fWordsPerClassifier = defaultdict(lambda: 0)
+        self.nWordsPerClassifier = defaultdict(lambda: 0)
+        self.nVocabsPerClassifier = defaultdict(lambda: 0)
+
+        # TODO
+        # Implement a multinomial naive bayes classifier and a naive bayes classifier with boolean features. The flag
+        # naiveBayesBool is used to signal to your methods that boolean naive bayes should be used instead of the usual
+        # algorithm that is driven on feature counts. Remember the boolean naive bayes relies on the presence and
+        # absence of features instead of feature counts.
+
+        # When the best model flag is true, use your new features and or heuristics that are best performing on the
+        # training and test set.
+
+        # If any one of the flags filter stop words, boolean naive bayes and best model flags are high, the other two
+        # should be off. If you want to include stop word removal or binarization in your best performing model, you
+        # will need to write the code accordingly.
+
+    def classify(self, words):
+        """
+        Classify a list of words and return a positive or negative sentiment
+        """
+        if self.stopWordsFilter:
+            words = self.filterStopWords(words)
+
+        if self.naiveBayesBool:
+            words = set(words)
+
+        # TODO
+        # classify a list of words and return the 'pos' or 'neg' classification
+        # Write code here
+        classifier = 'pos'
+
+        negate = False
+        turn = False
+
+        if self.bestModel:
+            for index, w in enumerate(words):
+                w = w.strip(punctuations)
+                words[index] = w
+                
+                if w in punctuations:
+                    negate = False
+                    turn = False
+                if turn == True:
+                    words[index] = 'turn_' + w
+                if negate == True:
+                    words[index] = 'not_' + w
+                if w in turnWords:
+                    turn = True
+                if w in negWords:
+                    negate = True
+                
+            words = set(words)
+
+            C_nb = float('Inf')
+            
+            alpha = 0.6
+
+            for c in self.classifiers.keys():
+                P_cj = self.classifiers[c] / float(self.nDocuments)
+
+                newC_nb = -math.log(P_cj)
+
+                for w in words:
+                    P_wi_cj = (self.fWordsPerClassifier[(w, c)] + alpha) / float(self.nWordsPerClassifier[c] + self.nVocabsPerClassifier[c] * alpha)
+                    newC_nb += -math.log(P_wi_cj)
+                if newC_nb < C_nb:
+                    C_nb = newC_nb
+                    classifier = c
+        else:
+            C_nb = float('Inf')
+            
+            alpha = 2
+
+            if self.naiveBayesBool:
+                alpha = 0.38
+
+            for c in self.classifiers.keys():
+                P_cj = self.classifiers[c] / float(self.nDocuments)
+
+                newC_nb = -math.log(P_cj)
+
+                for w in words:
+                    P_wi_cj = (self.fWordsPerClassifier[(w, c)] + alpha) / float(self.nWordsPerClassifier[c] + self.nVocabsPerClassifier[c] * alpha)
+                    newC_nb += -math.log(P_wi_cj)
+                if newC_nb < C_nb:
+                    C_nb = newC_nb
+                    classifier = c
+
+        return classifier
+
+    def addDocument(self, classifier, words):
+        """
+        Train your model on a document with label classifier (pos or neg) and words (list of strings). You should
+        store any structures for your classifier in the naive bayes class. This function will return nothing
+        """
+        # TODO
+        # Train model on document with label classifiers and words
+        # Write code here
+        if self.naiveBayesBool:
+            words = set(words)
+
+        negate = False
+        turn = False
+
+        if self.bestModel:
+            for index, w in enumerate(words):
+                w = w.strip(punctuations)
+                words[index] = w
+
+                if w in punctuations:
+                    negate = False
+                    turn = False
+                if turn == True:
+                    words[index] = 'turn_' + w
+                if negate == True:
+                    words[index] = 'not_' + w
+                if w in turnWords:
+                    turn = True
+                if w in negWords:
+                    negate = True
+
+            words = set(words)
+
+        # Frequency of classifiers
+        self.classifiers[classifier] += 1
+
+        # Number of documents classified
+        self.nDocuments += 1
+
+        for w in words:
+            # Number of words per classifier
+            self.nWordsPerClassifier[classifier] += 1
+
+            # Number of vocabs per classifier
+            if self.fWordsPerClassifier[(w, classifier)] == 0:
+                self.nVocabsPerClassifier[classifier] += 1
+
+            # Frequency of words per classifier
+            self.fWordsPerClassifier[(w, classifier)] += 1
+
+    def readFile(self, fileName):
+        """
+        Reads a file and segments.
+        """
+        contents = []
+        f = open(fileName)
+        for line in f:
+            contents.append(line)
+        f.close()
+        str = '\n'.join(contents)
+        result = str.split()
+        return result
+
+    def trainSplit(self, trainDir):
+        """Takes in a trainDir, returns one TrainSplit with train set."""
+        split = self.TrainSplit()
+        posDocTrain = os.listdir('%s/pos/' % trainDir)
+        negDocTrain = os.listdir('%s/neg/' % trainDir)
+        for fileName in posDocTrain:
+            doc = self.Document()
+            doc.words = self.readFile('%s/pos/%s' % (trainDir, fileName))
+            doc.classifier = 'pos'
+            split.train.append(doc)
+        for fileName in negDocTrain:
+            doc = self.Document()
+            doc.words = self.readFile('%s/neg/%s' % (trainDir, fileName))
+            doc.classifier = 'neg'
+            split.train.append(doc)
+        return split
+
+    def train(self, split):
+        for doc in split.train:
+            words = doc.words
+            if self.stopWordsFilter:
+                words = self.filterStopWords(words)
+            self.addDocument(doc.classifier, words)
+
+    def crossValidationSplits(self, trainDir):
+        """Returns a lsit of TrainSplits corresponding to the cross validation splits."""
+        splits = []
+        posDocTrain = os.listdir('%s/pos/' % trainDir)
+        negDocTrain = os.listdir('%s/neg/' % trainDir)
+        # for fileName in trainFileNames:
+        for fold in range(0, self.numFolds):
+            split = self.TrainSplit()
+            for fileName in posDocTrain:
+                doc = self.Document()
+                doc.words = self.readFile('%s/pos/%s' % (trainDir, fileName))
+                doc.classifier = 'pos'
+                if fileName[2] == str(fold):
+                    split.test.append(doc)
+                else:
+                    split.train.append(doc)
+            for fileName in negDocTrain:
+                doc = self.Document()
+                doc.words = self.readFile('%s/neg/%s' % (trainDir, fileName))
+                doc.classifier = 'neg'
+                if fileName[2] == str(fold):
+                    split.test.append(doc)
+                else:
+                    split.train.append(doc)
+            yield split
+
+    def test(self, split):
+        """Returns a list of labels for split.test."""
+        labels = []
+        for doc in split.test:
+            words = doc.words
+            if self.stopWordsFilter:
+                words = self.filterStopWords(words)
+            guess = self.classify(words)
+            labels.append(guess)
+        return labels
+
+    def buildSplits(self, args):
+        """
+        Construct the training/test split
+        """
+        splits = []
+        trainDir = args[0]
+        if len(args) == 1:
+            print '[INFO]\tOn %d-fold of CV with \t%s' % (self.numFolds, trainDir)
+
+            posDocTrain = os.listdir('%s/pos/' % trainDir)
+            negDocTrain = os.listdir('%s/neg/' % trainDir)
+            for fold in range(0, self.numFolds):
+                split = self.TrainSplit()
+                for fileName in posDocTrain:
+                    doc = self.Document()
+                    doc.words = self.readFile('%s/pos/%s' % (trainDir, fileName))
+                    doc.classifier = 'pos'
+                    if fileName[2] == str(fold):
+                        split.test.append(doc)
+                    else:
+                        split.train.append(doc)
+                for fileName in negDocTrain:
+                    doc = self.Document()
+                    doc.words = self.readFile('%s/neg/%s' % (trainDir, fileName))
+                    doc.classifier = 'neg'
+                    if fileName[2] == str(fold):
+                        split.test.append(doc)
+                    else:
+                        split.train.append(doc)
+                splits.append(split)
+        elif len(args) == 2:
+            split = self.TrainSplit()
+            testDir = args[1]
+            print '[INFO]\tTraining on data set:\t%s testing on data set:\t%s' % (trainDir, testDir)
+            posDocTrain = os.listdir('%s/pos/' % trainDir)
+            negDocTrain = os.listdir('%s/neg/' % trainDir)
+            for fileName in posDocTrain:
+                doc = self.Document()
+                doc.words = self.readFile('%s/pos/%s' % (trainDir, fileName))
+                doc.classifier = 'pos'
+                split.train.append(doc)
+            for fileName in negDocTrain:
+                doc = self.Document()
+                doc.words = self.readFile('%s/neg/%s' % (trainDir, fileName))
+                doc.classifier = 'neg'
+                split.train.append(doc)
+
+            posDocTest = os.listdir('%s/pos/' % testDir)
+            negDocTest = os.listdir('%s/neg/' % testDir)
+            for fileName in posDocTest:
+                doc = self.Document()
+                doc.words = self.readFile('%s/pos/%s' % (testDir, fileName))
+                doc.classifier = 'pos'
+                split.test.append(doc)
+            for fileName in negDocTest:
+                doc = self.Document()
+                doc.words = self.readFile('%s/neg/%s' % (testDir, fileName))
+                doc.classifier = 'neg'
+                split.test.append(doc)
+            splits.append(split)
+        return splits
+
+    def filterStopWords(self, words):
+        """
+        Stop word filter
+        """
+        removed = []
+        for word in words:
+            if not word in self.stopList and word.strip() != '':
+                removed.append(word)
+        return removed
+
+
+def test10Fold(args, stopWordsFilter, naiveBayesBool, bestModel):
+    nb = NaiveBayes()
+    splits = nb.buildSplits(args)
+    avgAccuracy = 0.0
+    fold = 0
+    for split in splits:
+        classifier = NaiveBayes()
+        classifier.stopWordsFilter = stopWordsFilter
+        classifier.naiveBayesBool = naiveBayesBool
+        classifier.bestModel = bestModel
+        accuracy = 0.0
+        for doc in split.train:
+            words = doc.words
+            classifier.addDocument(doc.classifier, words)
+
+        for doc in split.test:
+            words = doc.words
+            guess = classifier.classify(words)
+            if doc.classifier == guess:
+                accuracy += 1.0
+
+        accuracy = accuracy / len(split.test)
+        avgAccuracy += accuracy
+        print '[INFO]\tFold %d Accuracy: %f' % (fold, accuracy)
+        fold += 1
+    avgAccuracy = avgAccuracy / fold
+    print '[INFO]\tAccuracy: %f' % avgAccuracy
+
+
+def classifyFile(stopWordsFilter, naiveBayesBool, bestModel, trainDir, testFilePath):
+    classifier = NaiveBayes()
+    classifier.stopWordsFilter = stopWordsFilter
+    classifier.naiveBayesBool = naiveBayesBool
+    classifier.bestModel = bestModel
+    trainSplit = classifier.trainSplit(trainDir)
+    classifier.train(trainSplit)
+    testFile = classifier.readFile(testFilePath)
+    print classifier.classify(testFile)
+
+
+def main():
+    stopWordsFilter = False
+    naiveBayesBool = False
+    bestModel = False
+    (options, args) = getopt.getopt(sys.argv[1:], 'fbm')
+    if ('-f', '') in options:
+        stopWordsFilter = True
+    elif ('-b', '') in options:
+        naiveBayesBool = True
+    elif ('-m', '') in options:
+        bestModel = True
+
+    if len(args) == 2 and os.path.isfile(args[1]):
+        classifyFile(stopWordsFilter, naiveBayesBool, bestModel, args[0], args[1])
+    else:
+        test10Fold(args, stopWordsFilter, naiveBayesBool, bestModel)
+
+
+if __name__ == "__main__":
+    main()
